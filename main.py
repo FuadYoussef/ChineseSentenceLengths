19711500574d93092d389dc28fe3570fa9cf8d41
Fuad Youssef <fuadmyoussef@gmail.com>
2021-05-20 20:17:56 -0400

    have mfdfa working

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ from MFDFA import MFDFA
 
 def multifractal(sls):
     data = np.array(sls)
-    # Select a band of lags, which usually ranges from
-    # very small segments of data, to very long ones, as
     lag = np.unique(np.logspace(0.5, 3, 100).astype(int))
-    # Notice these must be ints, since these will segment
-    # the data into chucks of lag size
 
     # Select the power q
     q = 5
@@ -36,10 +32,42 @@ def multifractal(sls):
     plt.show()
     # Now what you should obtain is: slope = H + 1
 
+def mfdfatest(sls):
+    data = np.array(sls)
+    # Select a band of lags, which are ints
+    lag = np.unique(np.logspace(0.5, 3, 100).astype(int))
+
+    # Select a list of powers q
+    q_list = np.linspace(-4, 4, 9)
+    q_list = q_list[q_list != 0.0]
+
+    # The order of the polynomial fitting
+    order = 2
+
+    # Obtain the (MF)DFA as
+    lag, dfa = MFDFA(data, lag=lag, q=q_list, order=order)
+    # To uncover the Hurst index, lets get some log-log plots
+    plt.loglog(lag, dfa, 'o', label='fOU: MFDFA q=2')
+
+    # And now we need to fit the line to find the slope. We will
+    # fit the first points, since the results are more accurate
+    # there. Don't forget that if you are seeing in log-log
+    # scales, you need to fit the logs of the results
+    print(len(sls)//5)
+    slopes = np.polyfit(np.log(lag[20:len(sls)//5]), np.log(dfa[20:len(sls)//5]), 1)[0]
+    slopes = slopes.tolist()
+    hExponents = []
+    for slope in slopes:
+        hExponents.append(slope - 1)
+    print(hExponents)
+
+    # Now what you should obtain is: slope = H + 1
+    plt.show()
+
 if __name__ == '__main__':
     sentenceLengths = getSentenceLengths('乌云遇皎月')
-    multifractal(sentenceLengths)
-
+    # multifractal(sentenceLengths)
+    mfdfatest(sentenceLengths)
     # simple_sample(sentenceLengths)
 
 
